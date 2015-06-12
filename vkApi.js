96a3027e73cfd053b4b241ccd0f6aa74568fdcff
smartapant <smartapant@gmail.com>
2015-06-12 11:20:59 +0300

    Auth returned :)

--- vkApi.js
+++ vkApi.js
@@ -1,8 +1,7 @@
 
-var q = require('q');;
+var q = require('q');
 var VK = require('vksdk');
 var vk = new VK(config.vkSettings);
-var at;
 
 module.exports = {
 	
@@ -10,34 +9,31 @@ module.exports = {
 		var self = this;
 		return this.checkAccessToken(userId, accessToken)
 			.then(function() {
-                at = accessToken;
 				return self.getUserInfo(userId);
 			}, function(err) {
 				logger.error('Check access token: ' + err);
 			});
 	},
-	
-	checkAccessToken: function(userId, accessToken) {
-		var deferred = q.defer();
-		vk.request('users.get', {'access_token' : accessToken}, function(r) {
 
-            deferred.resolve();
-//			if (!r.error && r.response && r.response.length > 0) {
-//				if (r.response[0].id == userId) {
-//					deferred.resolve();
-//				} else {
-//					deferred.reject('Incorrect access token for user: %d', userId);
-//				}
-//			} else {
-//				deferred.reject(r.error);
-//			}
-		});
-		return deferred.promise;
-	},
+    checkAccessToken: function(userId, accessToken) {
+        var deferred = q.defer();
+        this.secureRequest('secure.checkToken', {'token' : accessToken}, function(r) {
+            if (!r.error && r.response) {
+                if (r.response.user_id == userId) {
+                    deferred.resolve();
+                } else {
+                    deferred.reject('Incorrect access token for user: %d', userId);
+                }
+            } else {
+                deferred.reject(r.error);
+            }
+        });
+        return deferred.promise;
+    },
 	
 	getUserInfo: function(id) {
 		var deferred = q.defer();
-		vk.request('users.get', {access_token: at, 'user_id' : id, 'fields': 'id, first_name, sex, bdate'}, function(r) {
+		vk.request('users.get', {'user_id' : id, 'fields': 'id, first_name, sex, bdate'}, function(r) {
 			if (!r.error && r.response && r.response.length > 0) {
 				if (!r.response[0].deactivated) {
 					deferred.resolve(r.response[0]);
@@ -61,5 +57,29 @@ module.exports = {
 			}
 		});
 		return deferred.promise;
-	}
+	},
+
+    secureRequest: function(_method, _requestParams, _response) {
+        vk.setSecureRequests(true);
+        vk.request(_method, _requestParams, _response);
+        vk.setSecureRequests(false);
+    },
+
+    setServerToken: function() {
+        var deferred = q.defer();
+        vk.requestServerToken(function (res) {
+            vk.setToken(res.access_token);
+            deferred.resolve(res.access_token);
+        });
+        return deferred.promise;
+    },
+
+    init: function () {
+        var deferred = q.defer;
+        this.setServerToken()
+            .then(function () {
+                deferred.resolve();
+            });
+        return deferred.promise;
+    }
 }
