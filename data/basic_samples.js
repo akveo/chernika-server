be048926e6d6dbd1b900ee5f8f7d3702d5365e7f
smartapant <smartapant@gmail.com>
2015-09-07 09:32:55 +0300

    Data scripts useless requires.

--- data/basic_samples.js
+++ data/basic_samples.js
@@ -1,12 +1,6 @@
 (function () {
-    GLOBAL["config"] =  require('./../config.local.js');
     var models = require('./../api/model');
-    var utils = require('./../utils');
     var dbutils = require('./utils');
-    var UserService = require('./../api/services/UserService');
-    var ChatService = require('./../api/services/UserService');
-    var q = require('q');
-    var vkApi = require('./../vkApi.js');
 
     var coords = [27.507375, 53.883873];  //Dziarzhynskogo av.
     var vkIds = [1, 2288280, 17197491, 58513866, 15037767, 14559720, 10249179, 18802294, 26139084, 21162999, 3863981, 6135811, 10846589, 16704573];
--- data/utils.js
+++ data/utils.js
@@ -1,11 +1,8 @@
 (function() {
   GLOBAL["config"] =  require('./../config.local.js');
-  var models = require('./../api/model');
   var utils = require('./../utils');
   var UserService = require('./../api/services/UserService');
-  var ChatService = require('./../api/services/UserService');
   var q = require('q');
-  var vkApi = require('./../vkApi.js');
 
   var messages = [
     {
