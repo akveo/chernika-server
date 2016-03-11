bb61db5a7cdec7491c12bed5a652bf99e729c9ee
alex <demeshko.alexander.v@gmail.com>
2016-03-11 14:22:42 +0300

    refactor(reports): add timestamp for report scheme

--- api/models/Report.js
+++ api/models/Report.js
@@ -4,6 +4,6 @@ var Schema = mongoose.Schema;
 var reportSchema = new Schema({
   user: {type: Schema.Types.ObjectId, ref:'User'},
   target : {type: Schema.Types.ObjectId, ref:'User'}
-});
+}, { timestamps: { createdAt: 'created'}});
 
 module.exports = reportSchema;
